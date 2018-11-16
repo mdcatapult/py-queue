5bc2409298381fe532f46d37e40089a369e7a699
Matt Cockayne <matthew.cockayne@md.catapult.org.uk>
2018-11-16 15:21:30 +0000

    adds guards for requeue and error in publisher

--- klein_queue/rabbitmq/publisher.py
+++ klein_queue/rabbitmq/publisher.py
@@ -6,15 +6,20 @@ from klein_config import config
 from .synchronous.publisher import Publisher
 
 DOWNSTREAM = None
+UPSTREAM = None
+ERROR = None
+
 if config.has("publisher"):
   DOWNSTREAM = Publisher(config.get("publisher"))
   DOWNSTREAM.connect()
 
-UPSTREAM = Publisher(config.get("consumer"))
-UPSTREAM.connect()
+if config.has("consumer"):
+  UPSTREAM = Publisher(config.get("consumer"))
+  UPSTREAM.connect()
 
-ERROR = Publisher(config.get('error'))
-ERROR.connect()
+if config.has("error"):
+  ERROR = Publisher(config.get('error'))
+  ERROR.connect()
 
 
 def publish(message):
@@ -30,6 +35,8 @@ def requeue(message):
     '''
     publish message to same queue being consumed
     '''
+    if not UPSTREAM:
+      raise EnvironmentError("No upstream has been configured for publishing")
     UPSTREAM.publish(message)
 
 
@@ -37,4 +44,6 @@ def error(message):
     '''
     publish message to error queue
     '''
+    if not ERROR:
+      raise EnvironmentError("No error has been configured for publishing")
     ERROR.publish(message)
