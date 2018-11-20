79d303bc3c73951f301cb242e7ac0433fcaff31e
Matt Cockayne <matthew.cockayne@md.catapult.org.uk>
2018-11-20 17:53:29 +0000

    bumps version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='klein_queue',
-      version='0.1.6',
+      version='0.1.7',
       description='RabbitMQ integration',
       url='http://gitlab.mdcatapult.io/informatics/klein/klein_queue',
       author='Matt Cockayne',
