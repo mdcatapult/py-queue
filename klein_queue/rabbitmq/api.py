6e7fef440e801030572acd7941502e4821704884
Matt Cockayne <matthew.cockayne@md.catapult.org.uk>
2018-11-21 08:28:33 +0000

    adds very basic caching to list_queues

--- klein_queue/rabbitmq/api.py
+++ klein_queue/rabbitmq/api.py
@@ -1,14 +1,32 @@
 # -*- coding: utf-8 -*-
+import time
 import requests
 from klein_config import config
 
+# TODO: Implement more robust distribiuted caching mechanism
 
-def list_queues(exchange):
+ttl = config.get('rabbitmq.api.list_queues.cache', 0)
+cache = {}
+
+
+def list_queues(exchange, flush=False):
     '''
     utility to retrive queues attached to exchange
     configured user for connection shoudl have
     management permissions
     '''
+
+    if flush:
+        del cache[exchange]
+
+    if exchange in cache:
+        if "timestamp" in cache[exchange]:
+            diff = time.time() - cache[exchange]["timestamp"]
+            if diff >= ttl:
+                del cache[exchange]
+        if "queues" in cache[exchange]:
+            return cache[exchange]["queues"]
+
     endpoint = "/api/exchanges/%%2f/%s/bindings/source" % exchange
     url = 'http://%s:%s%s' % (
         config.get("rabbitmq.host"),
@@ -22,4 +40,10 @@ def list_queues(exchange):
     )
     queues = [q["destination"]
               for q in response.json() if q["destination_type"] == "queue"]
+
+    cache[exchange] = {
+        "queues": queues,
+        "timestamp": time.time()
+    }
+
     return queues
