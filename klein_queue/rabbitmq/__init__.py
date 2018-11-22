86acf8cfc06c56a113e48619f315c8f3f12a9a8a
Matt Cockayne <matthew.cockayne@md.catapult.org.uk>
2018-11-22 11:54:19 +0000

    adds ack,nack & nackError to __init__

--- klein_queue/rabbitmq/__init__.py
+++ klein_queue/rabbitmq/__init__.py
@@ -3,6 +3,9 @@ klein_queue.rabbit
 '''
 
 from .consumer import consume
+from .consumer import ack
+from .consumer import nack
+from .consumer import nackError
 from .api import list_queues
 from .publisher import publish
 from .publisher import requeue
