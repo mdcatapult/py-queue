b018c880d16d3b8887446ec9841a642801cdbc99
Matt Cockayne <matthew.cockayne@md.catapult.org.uk>
2018-11-22 11:29:21 +0000

    adds some custom convienience functions to curry handler for custom ack

--- klein_queue/rabbitmq/consumer.py
+++ klein_queue/rabbitmq/consumer.py
@@ -2,8 +2,40 @@
 '''
 klein_queue_rabbitmq.consumer
 '''
+import time
 from klein_config import config
 from .asynchronous.consumer import Consumer
+
+
+def nack(msg):
+    '''
+    Covienience currid function to Negative Acknowledge message
+    '''
+    def handle(consumer, channel, envelope, properties):
+        # pylint: disable=unused-argument
+        print(time.time(), "NACK: ", envelope.delivery_tag, msg)
+        channel.basic_nack(envelope.delivery_tag)
+    return handle
+
+def ack(msg):
+    '''
+    Covienience currid function to Acknowledge message
+    '''
+    def handle(consumer, channel, envelope, properties):
+        # pylint: disable=unused-argument
+        print(time.time(), "ACK: ", envelope.delivery_tag, msg)
+        channel.basic_ack(envelope.delivery_tag)
+    return handle
+
+def nackError(err):
+    '''
+    Covienience currid function to Negative Acknowledge message with error
+    '''
+    def handle(consumer, channel, basic_deliver, properties):
+        # pylint: disable=unused-argument
+        print(time.time(), "ERROR: ", str(err))
+        channel.basic_nack(basic_deliver.delivery_tag)
+    return handle
 
 
 def consume(callback):
