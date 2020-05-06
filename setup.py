18252d335367d314985b9632d36e22f148fb0dfe
daniel.james <daniel.james@md.catapult.org.uk>
2020-05-06 14:51:18 +0100

    increment the version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='klein_queue',
-      version='0.2.11',
+      version='0.2.12',
       description='RabbitMQ integration',
       url='http://gitlab.mdcatapult.io/informatics/klein/klein_queue',
       author='Matt Cockayne',
