9987a0e243cab84907ce33e3ef8c7e551ea2978a
Mark Laing <mark.laing@md.catapult.org.uk>
2020-07-16 17:23:30 +0100

    Further test modifications.

--- src/klein_queue/rabbitmq/util.py
+++ src/klein_queue/rabbitmq/util.py
@@ -8,7 +8,13 @@ class KleinQueueError(Exception):
     '''
     def __init__(self, *args, body=None):
         self.body = body
+        self.msg = "KleinQueueError unknown"
+        if len(args) > 0:
+            self.msg = args[0]
         super().__init__(self, *args)
+
+    def __str__(self):
+        return str(self.msg)
 
 
 def get_url_parameters(conf):
