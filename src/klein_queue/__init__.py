36d83d3752affa80dad2e426131d5fbed287f50f
Mark Laing <mark.laing@md.catapult.org.uk>
2020-07-31 08:57:17 +0100

    Changes requeue function to take a publisher as an argument.

--- src/klein_queue/__init__.py
+++ src/klein_queue/__init__.py
@@ -3,4 +3,4 @@ klein_queue
 '''
 from .consumer import rabbit_consume
 from .publisher import rabbit_publish
-from .publisher import rabbit_requeue
+from .publisher import requeue
--- src/klein_queue/publisher.py
+++ src/klein_queue/publisher.py
@@ -13,13 +13,17 @@ LOGGER = logging.getLogger(__name__)
 def rabbit_publish(config, key, data):
     '''
     Publish data to queue defined on config key
+
+    NOTE: This is a convenience function. Each call will create a new connection to rabbit.
+    Use the Publisher class for a persistent connection.
     '''
     publish(config, key, data)
 
 
-def rabbit_requeue(config, key, data, on_limit_reached=None, **kwargs):
+def requeue(publisher, config, data, on_limit_reached=None, **kwargs):
     '''
-    publishes data to queue defined on config key.
+    convenience function handles requeue logic before publishing the
+    given data with the given publisher
     executes callback on data if requeue limit is both found and exceeded.
 
     :keyword on_limit_reached -- Callback to execute on data if requeue limit is reached.
@@ -40,4 +44,4 @@ def rabbit_requeue(config, key, data, on_limit_reached=None, **kwargs):
         if on_limit_reached:
             on_limit_reached(data)
     else:
-        publish(config, key, data)
+        publisher.publish(data)
