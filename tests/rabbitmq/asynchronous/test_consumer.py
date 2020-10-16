5aa70c12ef49afba159bd3e814052ff34ee7979e
Matthew Walton <matthew.walton@md.catapult.org.uk>
2020-10-16 12:01:37 +0100

    Added concurrency test

--- tests/rabbitmq/asynchronous/test_consumer.py
+++ tests/rabbitmq/asynchronous/test_consumer.py
@@ -1,6 +1,7 @@
 import argparse
 import threading
 from unittest import mock
+from random import randint
 import time
 
 class CustomThrowable(Exception):
@@ -29,16 +30,16 @@ class TestConsumer:
                 "password": "doclib",
             },
             "consumer": {
-                "queue": "pytest",
+                "queue": "pytest.consume",
                 "auto_acknowledge": True,
                 "prefetch": 1,
                 "create_on_connect": True,
             },
             "publisher": {
-                "queue": "pytest"
+                "queue": "pytest.consume"
             }
         })
-
+      
         from src.klein_queue.rabbitmq.asynchronous.consumer import Consumer
         consumer = Consumer(config, "consumer")
         consumer.set_handler(handle_handle(consumer))
@@ -57,3 +58,55 @@ class TestConsumer:
 
         consumer.stop()
 
+    
+    def test_worker_concurrency(self):
+        workers = randint(2, 5)
+        self._events = []
+
+        def handler_fn(msg, **kwargs):
+            event_id = msg['event']
+            self._events[event_id].set()
+            time.sleep(10) # sleep to block this worker
+
+        from klein_config.config import EnvironmentAwareConfig
+        config = EnvironmentAwareConfig({
+            "rabbitmq": {
+                "host": ["localhost"],
+                "port": 5672,
+                "username": "doclib",
+                "password": "doclib",
+            },
+            "consumer": {
+                "queue": "pytest.concurrency",
+                "auto_acknowledge": True,
+                "prefetch": workers,
+                "create_on_connect": True,
+            },
+            "publisher": {
+                "queue": "pytest.concurrency"
+            }
+        })
+
+        from src.klein_queue.rabbitmq.asynchronous.consumer import Consumer
+        consumer = Consumer(config, "consumer", handler_fn, workers)
+
+        # check number of threads spawned
+        assert len(consumer._workers) == workers
+
+        c = threading.Thread(target=consumer.run)
+        c.start()
+
+        from src.klein_queue.rabbitmq.synchronous.publisher import Publisher
+        publisher = Publisher(config, "publisher")
+        publisher.connect()
+
+        for i in range(workers):
+            # send one message for each worker
+            self._events.append(threading.Event())
+            publisher.publish_message({'event': i})
+
+        for i in range(workers):
+            message_received_in_time = self._events[i].wait(5)
+            assert message_received_in_time
+
+        consumer.stop()
