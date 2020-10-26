b3493d95c8cbee56252642fd3c6f06cbb78e097a
Matthew Walton <matthew.walton@md.catapult.org.uk>
2020-10-26 16:49:01 +0000

    Moved docstring to __init__ in Publisher@

--- src/klein_queue/rabbitmq/publisher.py
+++ src/klein_queue/rabbitmq/publisher.py
@@ -13,51 +13,52 @@ class Publisher(Thread):
 
     We use a multithreaded publisher to keep the I/O loop (and heartbeat) alive and maintain a persistent connection.
     This is more efficient than creating a new connection for every message.
-
-    `config`: The `klein_config.config.EnvironmentAwareConfig` containing connection details to rabbit.
-
-    `key`: The `str` key in the config with specific publisher config, these are:
-    ```yaml
-    key:                            # i.e. upstream
-        queue: 'queue name'         # The name of the rabbitmq queue.
-        create_on_connect: true     # Whether to create a queue on connection.
-    ```
-    ## Example
-    **main.py**
-    ```python
-    from klein_config.config import EnvironmentAwareConfig
-    from src.klein_queue.rabbitmq.consumer import Consumer
-
-    config = EnvironmentAwareConfig()       # Read from file specified with `--config`
-    def handle_fn(message, **kwargs):       # handler_fn to be called in worker threads.
-        print(message)
-    consumer = Consumer(config, "consumer", handler_fn)
-    consumer.run()
-    ```
-    **config.yaml**
-    ```python
-    rabbitmq:
-        host: [localhost]
-        port: 5672
-        username: guest
-        password: guest
-        heartbeat: 2
-    consumer:
-        name: test.consumer
-        queue: test
-        auto_acknowledge: false
-        prefetch: 2
-        create_on_connect: true
-        error: error
-        workers: 2
-    ```
-    **terminal**
-    ```bash
-    python main.py --config config.yaml
-    ```
     """
 
     def __init__(self, config, key):
+        """
+        `config`: The `klein_config.config.EnvironmentAwareConfig` containing connection details to rabbit.
+
+        `key`: The `str` key in the config with specific publisher config, these are:
+        ```yaml
+        key:                            # i.e. upstream
+            queue: 'queue name'         # The name of the rabbitmq queue.
+            create_on_connect: true     # Whether to create a queue on connection.
+        ```
+        ## Example
+        **main.py**
+        ```python
+        from klein_config.config import EnvironmentAwareConfig
+        from src.klein_queue.rabbitmq.consumer import Consumer
+
+        config = EnvironmentAwareConfig()       # Read from file specified with `--config`
+        def handle_fn(message, **kwargs):       # handler_fn to be called in worker threads.
+            print(message)
+        consumer = Consumer(config, "consumer", handler_fn)
+        consumer.run()
+        ```
+        **config.yaml**
+        ```python
+        rabbitmq:
+            host: [localhost]
+            port: 5672
+            username: guest
+            password: guest
+            heartbeat: 2
+        consumer:
+            name: test.consumer
+            queue: test
+            auto_acknowledge: false
+            prefetch: 2
+            create_on_connect: true
+            error: error
+            workers: 2
+        ```
+        **terminal**
+        ```bash
+        $ python main.py --config config.yaml
+        ```
+        """
         self._publisher = _PublisherWorker(config, key)
         super().__init__()
 
