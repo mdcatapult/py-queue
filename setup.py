84070254c7585801d85047cbf89d2133ac29f4f1
daniel.james <daniel.james@md.catapult.org.uk>
2021-01-14 09:14:48 +0000

    Adds requests back to setup.py & requirements.txt

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(name='klein_queue',
       package_dir={'': 'src'},
       install_requires=[
           'klein_config>=3.0.0,<4.0.0',
-          'pika>=1.1.0,<2.0.0'
+          'pika>=1.1.0,<2.0.0',
+          'requests>=2.23.0,<3.0.0'
       ],
       zip_safe=True)
