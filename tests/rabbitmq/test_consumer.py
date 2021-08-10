89fe0b11fe1056a65d01c45e955afd2fc6f15427
Roman Ma <roman.ma@md.catapult.org.uk>
2021-08-10 16:05:14 +0100

    add test for empty queue callback

--- tests/rabbitmq/test_consumer.py
+++ tests/rabbitmq/test_consumer.py
@@ -264,3 +264,45 @@ class TestConsumer:
         assert "forced error" in error_properties.headers['x-stack-trace']
         assert error_properties.headers["x-original-routing-key"] == "pytest.exceptions"
         assert error_properties.headers["x-original-exchange"] == ""
+
+    def test_on_empty_queue_callback(self):
+        event = threading.Event()
+
+        def handle_handle(cons):
+            def handler_fn(msg, properties=None, **kwargs):
+                pass
+            return handler_fn
+
+        def on_empty_queue(tracker=[]):  # make use of shared instance of list
+            event.set()
+            tracker.append(1)
+            assert len(tracker) == 1  # Run the first time only
+
+        config = EnvironmentAwareConfig({
+            **test_config,
+            "consumer": {
+                "queue": "pytest.consume",
+                "auto_acknowledge": True,
+                "create_on_connect": True
+            },
+            "publisher": {
+                "queue": "pytest.consume"
+            }
+        })
+
+        consumer = Consumer(config, "consumer", on_empty_queue=on_empty_queue)
+        consumer.set_handler(handle_handle(consumer))
+
+        c = threading.Thread(target=consumer.run)
+        c.start()
+
+        publisher = Publisher(config, "publisher")
+        publisher.start()
+        publisher.publish({'msg': 'test_message'})
+
+        # timeout = 10 seconds on waiting for message to arrive and then hit empty queue
+        message_received_in_time = event.wait(10)
+        assert message_received_in_time
+
+        consumer.stop()
+        publisher.stop()
